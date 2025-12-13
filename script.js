204ebc0456d7add130857fa63055db44d3099564
erpicoding <erik.pischke.bs@icloud.com>
2025-12-13 19:49:40 +0100

    add show nvs url parameter

--- script.js
+++ script.js
@@ -44,6 +44,19 @@ async function setLanguage(language) {
       });
     });
 }
+const parameters = new URL(window.location.href).searchParams;
+const entries = new URLSearchParams(parameters).entries();
+const entriesArray = Array.from(entries);
+
+entriesArray.forEach((entry) => {
+  let key = entry[0];
+
+  if (key == "nvs") {
+    document.getElementById("nvsProject").removeAttribute("hidden");
+  }
+});
+
+//HA-Modal
 
 // const ha = document.getElementById("ha");
 // if (localStorage.getItem("openHA") == null) {
